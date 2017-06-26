bb4d74d54c9f263e6a0231c1362018df45120c6e
Robin Newhouse <robin.newhouse@cern.ch>
2017-06-26 05:41:04 -0700

    Perfect match for preprocessing and NN prediction for 9000 events.

--- run/compare.py
+++ run/compare.py
@@ -14,24 +14,24 @@ def display_side_by_side(pre_py_file, pre_c_file):
     pre_py = (np.load(pre_py_file))["data"]
     pre_py = pre_py[pre_py[:,get_column_no["jet pt"]].argsort()] # sort both by pt
 
-    pre_c = np.loadtxt(open(pre_c_file, "rb"), delimiter=",", usecols=range(50))
+    pre_c = np.loadtxt(open(pre_c_file, "rb"), delimiter=",", usecols=range(51))
     # np.savez(pre_c_file+".npz", pre_c)
     # pre_c = (np.load(pre_c_file+".npz"))["arr_0"]   
     pre_c = pre_c[pre_c[:,get_column_no["jet pt"]].argsort()] # sort both by pt
 
-    for x in range(1000):
+    for x in range(len(pre_py)):
         vals_to_print = list(chain([0,1,3,4,5], range(20,len(pre_c[x]))))
 
         print("py:", end='')    
         for y in vals_to_print:
             p = pre_py[x][y]
-            print("{:.2e}".format(p), end=' ')
+            print("{:.5e}".format(p), end=' ')
         print()
 
         print("c: ", end='')    
         for y in vals_to_print:
             c = pre_c[compare_dict[x]][y]
-            print("{:.2e}".format(c), end=' ')
+            print("{:.5e}".format(c), end=' ')
         print()
         print("df:", end='')    
         for y in vals_to_print:
@@ -41,7 +41,7 @@ def display_side_by_side(pre_py_file, pre_c_file):
                 rel_err = float("inf")
                 if c == 0: rel_err = 0
             else: rel_err = (p - c)/p
-            print("{:.2e}".format(rel_err), end=' ')
+            print("{:.5e}".format(rel_err), end=' ')
             if abs(rel_err) >= d_err:
                 print("<-UNMATCHED", end=' ')
                 # sleep(0.5)
@@ -137,24 +137,36 @@ def compare(pre_py_file, pre_c_file):
 
 # for x in range(len(pre_py)):
 
-def get_compare_dict():
+def get_compare_dict(pre_py_file, pre_c_file):
+    # print(pre_c_file)
+    # pre_py = (np.load(pre_py_file))["data"]
+    # pre_py = pre_py[pre_py[:,get_column_no["jet pt"]].argsort()] # sort both by pt
+
+    # pre_c = np.loadtxt(open(pre_c_file, "rb"), delimiter=",", usecols=range(51))
+    # # np.savez(pre_c_file+".npz", pre_c)
+    # # pre_c = (np.load(pre_c_file+".npz"))["arr_0"]   
+    # pre_c = pre_c[pre_c[:,get_column_no["jet pt"]].argsort()] # sort both by pt
+
     # compare_dict = {}
-    #
+    
     # for x in range(len(pre_py)):
+    # # for x in range(100): # for testing
+    #     # print()
     #     for y in range(len(pre_c)):
-    #         if (abs(pre_py[x][get_column_no["jet pt"]] - pre_c[y][get_column_no["jet pt"]]/1000.0)) <= d_pt:
+    #     # for y in range(1000): # for testing
+    #         if (abs(pre_py[x][get_column_no["jet pt"]] - pre_c[y][get_column_no["jet pt"]])) <= d_pt:
     #             if(abs(pre_py[x][get_column_no["jet eta"]] - pre_c[y][get_column_no["jet eta"]] )) <= d_pt:
     #                 if (abs(pre_py[x][get_column_no["jet phi"]] - pre_c[y][get_column_no["jet phi"]])) <= d_pt:
     #                     # print("match")
     #                     compare_dict[x] = y
     #                     break
-    # print(compare_dict)
+    # # print(compare_dict)
 
     # np.save('event_map.npy', compare_dict)
     return np.load('event_map.npy').item()
 
 if __name__ == '__main__':
-    compare_dict = get_compare_dict()
+    compare_dict = get_compare_dict("npz_w_pred.npz", "jet_data_scored.csv")
     # display_py("testrun_short_10_no_scale_pt_no_scale_eta_phiflip_everything.npz")    
     # print()
     # display_py("testrun_short_10_pt_min_max_scale_pt_shift_prim_eta_phiflip_everything.npz")
@@ -166,6 +178,7 @@ if __name__ == '__main__':
     # display_side_by_side("testrun_short_10_pt_min_max_scale_pt_shift_prim_eta_phiflip_everything.npz", "jet_data_scaled_shifted.csv")
     # display_side_by_side("testrun_short_10_pt_min_max_scale_pt_shift_prim_and_rotate_prim_eta_phiflip_everything.npz", "jet_data_scaled_shifted_rotated.csv")
     # display_side_by_side("testrun_short_10_pt_min_max_scale_pt_shift_prim_and_rotate_prim_and_flip_eta_phiflip_everything.npz", "jet_data_scaled_shifted_rotated_flipped.csv")
-    display_side_by_side("testrun_short_10_pt_min_max_scale_pt_shift_prim_and_rotate_prim_and_flip_eta_phiflip_everything.npz", "jet_data_final.csv")
-    # display_side_by_side("npz_w_pred.npz", "jet_data_scored.csv")
+    # display_side_by_side("testrun_short_10_pt_min_max_scale_pt_shift_prim_and_rotate_prim_and_flip_eta_phiflip_everything.npz", "jet_data_final.csv")
+    display_side_by_side("npz_w_pred.npz", "jet_data_scored.csv")
+    # print(compare_dict)
 
