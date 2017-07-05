51a57636e796abf5f41091f4d111b7fde08dc0d7
Robin Newhouse <robin.newhouse@cern.ch>
2017-07-05 13:54:20 -0700

    Moved JAI dictionaries to their own directory

--- JetAnalysisInterfaces/JetAnalysisInterfaces/IJetSelectorTool.h
+++ JetAnalysisInterfaces/JetAnalysisInterfaces/IJetSelectorTool.h
@@ -1,4 +1,9 @@
 // for editors : this file is -*- C++ -*-
+
+/*
+  Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
+*/
+
 #ifndef JetAnalysisInterfaces_IJetSelectorTool_H_
 #define JetAnalysisInterfaces_IJetSelectorTool_H_
 
